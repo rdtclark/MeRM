6e85f42c05b38d4f6d45edde742899dac758692f
Robin Clark <robindtclark@gmail.com>
2020-07-22 08:48:02 +0100

    add show action to api

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,19 @@
 class PeopleController < ApplicationController
+  before_action :set_person
 
   def index
     people = Person.all
     render json: people
   end
+
+  def show
+    render json: @person
+  end
+
+  private 
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
 end
